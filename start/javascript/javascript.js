4cc220ee4f67ce8d666fdda27864ab4640033a03
GlauberFerreiraAngelo <42098584+GlauberFerreiraAngelo@users.noreply.github.com>
2022-06-23 19:56:03 -0300

    Add files via upload

---
+++ start/javascript/javascript.js
@@ -0,0 +1,49 @@
+const boneco = document.querySelector(".boneco");
+const garrafa = document.querySelector(".garrafaStartse");
+
+const jump = () => {
+    boneco.classList.add("jump-boneco");
+    setTimeout(()=>{
+        boneco.classList.remove("jump-boneco");
+    }, 500)
+};
+
+var quadroDePontos = document.querySelector(".pontos");
+var mensagens = document.querySelector(".mensagem");
+var pontos = 0;
+
+const loopGame = setInterval(() => {
+    
+    const garrafaPosition = garrafa.offsetLeft;
+    const bonecoPosition = +window
+                            .getComputedStyle(boneco)
+                            .bottom.replace("px", ""); 
+
+                                
+/*120  0  80*/
+    if(garrafaPosition <= 30 && garrafaPosition > 0 && bonecoPosition < 80){
+        garrafa.style.animation = "none";
+        garrafa.style.left = '${garrafaPosition}px';
+        
+        boneco.style.animation = "none";
+        boneco.style.bottom = '${bonecoPosition}px';
+
+        boneco.src = "./imagens/gameover1.png";
+        boneco.style.width = "100px";
+        boneco.style.marginLeft = bonecoPosition;
+
+        mensagens.innerHTML = "Beba água, tente outra vez.";
+           
+        clearInterval(loopGame);
+    }
+    
+    if(garrafaPosition > 80 && garrafaPosition > 0 && bonecoPosition < 80){   
+        pontos = pontos + 1;
+        quadroDePontos.innerHTML = pontos; 
+    }
+    
+
+    
+}, 10);
+
+document.addEventListener("keydown", jump);
